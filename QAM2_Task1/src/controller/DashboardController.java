4f5a83ee550c7a496ac2fe1c396d9a83fee9bd4a
Samantha <samanthack18@gmail.com>
2022-02-22 16:45:34 -0500

    customer layout and nav

--- QAM2_Task1/src/controller/DashboardController.java
+++ QAM2_Task1/src/controller/DashboardController.java
@@ -1,4 +1,23 @@
 package controller;
 
-public class DashboardController {
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class DashboardController extends SceneController{
+    public void OnAddCustomer(ActionEvent actionEvent) throws IOException {
+        changeScene(actionEvent, "/view/customer.fxml");
+    }
+
+    public void OnSelectedCustBtn(ActionEvent actionEvent) throws IOException {
+        changeScene(actionEvent, "/view/customer.fxml");
+    }
+
+    public void OnSelectedAppBtn(ActionEvent actionEvent) throws IOException {
+        changeScene(actionEvent, "/view/appointment.fxml");
+    }
+
+    public void OnAddApp(ActionEvent actionEvent) throws IOException {
+        changeScene(actionEvent, "/view/appointment.fxml");
+    }
 }
