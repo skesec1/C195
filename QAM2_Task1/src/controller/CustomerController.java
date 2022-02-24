b3a2096dcb2bbb780f9605c593c65bc1020ca806
Samantha <samanthack18@gmail.com>
2022-02-24 12:55:48 -0500

    IDE version change

--- QAM2_Task1/src/controller/CustomerController.java
+++ QAM2_Task1/src/controller/CustomerController.java
@@ -5,16 +5,13 @@ import javafx.event.ActionEvent;
 import java.io.IOException;
 
 public class CustomerController extends SceneController{
+    
+    //Dashboard button takes user to main Dashboard screen
     public void OnDashboard(ActionEvent actionEvent) throws IOException {
         changeScene(actionEvent, "/view/dashboard.fxml");
     }
 
-    public void OnAddCustomer(ActionEvent actionEvent) {
-    }
-
-    public void OnSelCustomerInfo(ActionEvent actionEvent) {
-    }
-
+    //Radio selections
     public void OnAllRadio(ActionEvent actionEvent) {
     }
 
@@ -24,9 +21,23 @@ public class CustomerController extends SceneController{
     public void OnMonthRadio(ActionEvent actionEvent) {
     }
 
+    //Customer form Buttons
     public void OnDeleteCustomer(ActionEvent actionEvent) {
     }
 
     public void OnSaveCustomer(ActionEvent actionEvent) {
+    }
+    
+    //Appointment form buttons
+    public void OnAddApp(ActionEvent actionEvent) throws IOException {
+        changeScene(actionEvent, "/view/appointment.fxml");
+    }
+
+    public void OnUpdateApp(ActionEvent actionEvent) throws IOException {
+        changeScene(actionEvent, "/view/appointment.fxml");
+    }
+
+    public void OnDelApp(ActionEvent actionEvent) throws IOException {
+        changeScene(actionEvent, "/view/appointment.fxml");
     }
 }
